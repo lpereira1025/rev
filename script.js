3054759fe6f688836223a8f1ccec6080ef56985c
Lucas Pereira <lpereira349@gmail.com>
2022-08-11 16:09:38 -0300

    rev

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome ="Jhon";
+        let sobrenome = "Doyle";
+        let currentTime = new Date();
+        let anoAtual = currentTime.getFullYear();
+        let idade = 31;
+        let anoNascimento = anoAtual - idade;
+        let idade2 = anoAtual - anoNascimento;
+        let cidade= "Curitiba";
+
+        console.log(`Eu sou ${nome} ${sobrenome} tenho ${idade2} anos e moro em ${cidade}.`);
